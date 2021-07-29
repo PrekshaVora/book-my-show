015f13807539b89d44106bbd8521e6fd5141d08b
unknown <vora.preksha2001@gmail.com>
2021-07-29 13:07:18 +0530

    TMDB api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import axios from "axios";
+
+// HOC
+import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
+
+// Pages
+import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
+import Plays from "./pages/Plays.page";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
+
 function App() {
   return (
-    <div className="App p-10">
-      
-      
-      <button className="border-2 px-2 py-1 border-blue-500 md:border-red-500 lg:border-green-500 rounded bg-blue-500 md:bg-red-500 lg:bg-green-500 text-white hover:bg-transparent hover:border-2 hover:text-blue-500 md:hover:text-red-500 lg:hover:text-green-500 focus:outline-none focus:ring focus:border-blue-300 md:focus:border-red-300 lg:focus:border-green-300">
-        Hello Tailwind</button>
-    </div>
+    <>
+      <DefaultHOC path="/" exact component={HomePage} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
+      <DefaultHOC path="/plays" exact component={Plays} />
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
+import "./index.css";
+import App from "./App";
+
+// context provider
+import MovieProvider from "./context/movie.context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MovieProvider>
+        <App />
+      </MovieProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
